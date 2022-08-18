3d15db3ca5555870e25bd905821a8abc58ccc5a3
dhruv-2015 <chadasaniyadhruv@gmail.com>
2022-08-18 11:41:32 +0530

    satab aatham

--- satam aatham/day-year.c
+++ satam aatham/day-year.c
@@ -2,7 +2,7 @@
 
 void main() {
     float day;
-    printf("enter length in centimeter");
+    printf("enter number of days");
     scanf("%f", &day);
     float year = day / 365;
     printf("%f year");
