51b485e577e43540fd647fefbf826a136f8135a2
dhruv-2015 <chadasaniyadhruv@gmail.com>
2022-09-30 13:49:53 +0530

    LAB 11

--- LAB 11/factorial.c
+++ LAB 11/factorial.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
-// 14 + 7 + 4 + 2 + 1 = 28
-// 28 can divided by 14  7  4  2  1 and sum of 14  7  4  2  1 is the same number
+
 
 void main()
 {
-    int i, num, sum = 0;
+    int i, num, sum = 1;
     printf("Enter any number to check perfect number: ");
     scanf("%d", &num);
-    printf("factors is");
+    printf("factorial is ");
     for(i = 1; i <= num; i++)
     {
-        if(num % i == 0)
-        {
-            printf(" %d", i);
-            // sum += i;
-        }
+       sum = sum * i;
     }
+    printf("%d", sum);
 }
