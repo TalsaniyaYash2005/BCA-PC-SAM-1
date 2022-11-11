7cc2a61a9eab2dec85846bf29d6fe746bcf79db6
Dhruv_Patle <chadasaniyadhruv@gmail.com>
2022-11-11 14:32:09 +0530

    update LAB 11

--- LAB 11/series1.c
+++ LAB 11/series1.c
@@ -14,6 +14,6 @@ void main() {
         
     }
     
-    printf("sum is %d ", sum);
+    printf("= %d ", sum);
 }
 
--- LAB 15/max-min-sum.c
+++ LAB 15/max-min-sum.c
@@ -2,17 +2,28 @@
 
 void main()
 {
-    int n;
+    int n, min, max, sum = 0;
     printf("how many number you want to enter: ");
     scanf("%d", &n);
 
     int arr[n];
-    int i;
-    for (i = 0; i < n; i++)
+
+    for (int i = 0; i < n; i++)
     {
         printf("enter %d number: ", i + 1);
         scanf("%d", &arr[i]);
     }
-    
-    
+    min = max = arr[0];
+
+    for (int i = 0; i < n; i++)
+    {
+        sum = sum + arr[i];
+        if (min > arr[i])
+            min = arr[i];
+        if (max < arr[i])
+            max = arr[i];
+    }
+    printf("\nmin number is : %d", min);
+    printf("\nmax number is : %d", max);
+    printf("\nsum is : %d", sum);
 }
